02efb05dfe04e26008a09f02bccf4c402b2f11ba
Chester Dayao <chesterd0328@gmail.com>
2026-07-30 03:33:30 +0800

    merged ivan2 to main: frontend updates

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from app import app
 import os
 
 if __name__ == '__main__':
-    debug_mode = os.getenv('FLASK_DEBUG', 'false').lower() == 'true'
+    debug_mode = os.getenv('FLASK_DEBUG', 'true').lower() == 'true'
     app.run(debug=debug_mode, port=5000)
